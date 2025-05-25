bf614e376a907797c00c1c89ce4f9f886a056bbd
Hardik-Soni12 <imailhr1k@gmail.com>
2025-05-25 21:25:14 +0530

    added 2nd method to directly convert numeric value to string to avoide error

--- 02.check_for_palindrome.py
+++ 02.check_for_palindrome.py
@@ -1,9 +1,14 @@
 def is_palindrome(n):
+    n = str(n) #meth2 for input by converting numeric value in str
     return n == n[::-1]
 
-expect_string = True
-get_input = lambda:input("Enter value to check whether it's a palindrome or not : ") if expect_string else int(input("Enter value to check whether it's a palindrome or not : "))
-value = str(get_input())
+# meth1 for input
+# expect_string = True
+# get_input = lambda:input("Enter value to check whether it's a palindrome or not : ") if expect_string else int(input("Enter value to check whether it's a palindrome or not : "))
+# value = str(get_input())
+
+# meth2 for input by 
+value = input("Enter the value of n: ")
     
 if is_palindrome(value):
     print(f"{value} is a Palindrome")
